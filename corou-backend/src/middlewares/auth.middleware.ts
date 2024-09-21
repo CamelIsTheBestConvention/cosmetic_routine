23886edbd183ece177e85225411c4b5501b90c0c
CamelIsTheBestConvention <ansdj3523@gmail.com>
2024-09-21 20:07:42 +0900

    complete edit address && delete address

--- corou-backend/src/middlewares/auth.middleware.ts
+++ corou-backend/src/middlewares/auth.middleware.ts
@@ -1,22 +1,29 @@
-import { Request, Response, NextFunction } from 'express';
-import { verifyToken } from '../utils/jwt.utils';
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt.utils";
 
 interface AuthenticatedRequest extends Request {
-    user?: any;
+  user?: any;
 }
 
-export const authMiddleware = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: '토큰이 제공되지 않았습니다.' });
-    }
+export const authMiddleware = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  console.log("오쓰헤더", authHeader);
+  if (!authHeader) {
+    return res.status(401).json({ message: "토큰이 제공되지 않았습니다." });
+  }
 
-    const token = authHeader.split(' ')[1];
-    try {
-        const decoded = verifyToken(token);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: '유효하지 않은 토큰입니다.' });
-    }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "유효하지 않은 토큰입니다.", error });
+  }
 };
--- corou-backend/src/services/address.service.ts
+++ corou-backend/src/services/address.service.ts
@@ -1,79 +1,107 @@
-import { Repository } from 'typeorm';
-import { Address } from '../entities/address.entity';
-import { UserService } from './user.service';
-import { injectable, inject } from 'tsyringe';
-import { REPOSITORY_TOKENS } from '../config/constants';
+import { Repository } from "typeorm";
+import { Address } from "../entities/address.entity";
+import { UserService } from "./user.service";
+import { injectable, inject } from "tsyringe";
+import { REPOSITORY_TOKENS } from "../config/constants";
 
 @injectable()
 export class AddressService {
-    constructor(
-        private userService: UserService,
-        @inject(REPOSITORY_TOKENS.AddressRepository) private addressRepository: Repository<Address>
-    ) { }
+  constructor(
+    private userService: UserService,
+    @inject(REPOSITORY_TOKENS.AddressRepository)
+    private addressRepository: Repository<Address>
+  ) {}
 
-    // 사용자 주소 추가 
-    async addAddress(user_key: number, name: string, addr: string, addr_detail: string, zip: string, tel: string, request: string, is_default: 'Y' | 'N'): Promise<Address> {
-        const foundUser = await this.userService.getUserByKey(user_key);
-        if (!foundUser) {
-            throw new Error('해당 유저를 찾을 수 없습니다.');
-        }
-        if (is_default === 'Y') {
-            await this.addressRepository.update({ user: foundUser }, { is_default: 'N' });
-        }
-        const newAddress = this.addressRepository.create({
-            user: foundUser,
-            name,
-            addr,
-            addr_detail,
-            zip,
-            tel,
-            request,
-            is_default
-        });
-        return await this.addressRepository.save(newAddress);
+  // 사용자 주소 추가
+  async addAddress(
+    user_key: number,
+    name: string,
+    addr: string,
+    addr_detail: string,
+    zip: string,
+    tel: string,
+    request: string,
+    is_default: "Y" | "N"
+  ): Promise<Address> {
+    const foundUser = await this.userService.getUserByKey(user_key);
+    if (!foundUser) {
+      throw new Error("해당 유저를 찾을 수 없습니다.");
     }
-
-    // 사용자 주소록 조회 
-    async getAllAddress(user_key: number): Promise<Address[]> {
-        const addresses = await this.addressRepository.find({ where: { user: { user_key } } });
-
-        return addresses;
+    if (is_default === "Y") {
+      await this.addressRepository.update(
+        { user: foundUser },
+        { is_default: "N" }
+      );
     }
+    const newAddress = this.addressRepository.create({
+      user: foundUser,
+      name,
+      addr,
+      addr_detail,
+      zip,
+      tel,
+      request,
+      is_default,
+    });
+    return await this.addressRepository.save(newAddress);
+  }
 
-    // 사용자 주소 조회
-    async getOneAddress(user_key: number, address_key: number): Promise<Address> {
-        const address = await this.addressRepository.findOneBy({ address_key });
-        if (!address) {
-            throw new Error('해당 주소를 찾을 수 없습니다.');
-        }
-        return address;
-    }
+  // 사용자 주소록 조회
+  async getAllAddress(user_key: number): Promise<Address[]> {
+    const addresses = await this.addressRepository.find({
+      where: { user: { user_key } },
+    });
 
-    // 사용자 주소 수정
-    async updateAddress(address_key: number, name: string, addr: string, addr_detail: string, zip: string, tel: string, request: string, is_default: 'Y' | 'N'): Promise<Address> {
-        const address = await this.addressRepository.findOneBy({ address_key });
-        if (!address) {
-            throw new Error('해당 주소를 찾을 수 없습니다.');
-        }
-        if (is_default === 'Y') {
-            await this.addressRepository.update({ user: address.user }, { is_default: 'N' });
-        }
-        address.name = name;
-        address.addr = addr;
-        address.addr_detail = addr_detail;
-        address.zip = zip;
-        address.tel = tel;
-        address.request = request;
-        address.is_default = is_default;
-        return await this.addressRepository.save(address);
-    }
+    return addresses;
+  }
 
-    // 사용자 주소 삭제
-    async deleteAddress(address_key: number): Promise<Address> {
-        const address = await this.addressRepository.findOneBy({ address_key });
-        if (!address) {
-            throw new Error('해당 주소를 찾을 수 없습니다.');
-        }
-        return await this.addressRepository.remove(address);
+  // 사용자 주소 조회
+  async getOneAddress(user_key: number, address_key: number): Promise<Address> {
+    const address = await this.addressRepository.findOneBy({ address_key });
+    if (!address) {
+      throw new Error("해당 주소를 찾을 수 없습니다.");
     }
+    return address;
+  }
+
+  // 사용자 주소 수정
+  async updateAddress(
+    address_key: number,
+    name: string,
+    addr: string,
+    addr_detail: string,
+    zip: string,
+    tel: string,
+    request: string,
+    is_default: "Y" | "N"
+  ): Promise<Address> {
+    const address = await this.addressRepository.findOneBy({ address_key });
+    if (!address) {
+      throw new Error("해당 주소를 찾을 수 없습니다.");
+    }
+    if (is_default === "Y") {
+      await this.addressRepository.update(
+        { user: address.user },
+        { is_default: "N" }
+      );
+    }
+    address.name = name;
+    address.addr = addr;
+    address.addr_detail = addr_detail;
+    address.zip = zip;
+    address.tel = tel;
+    address.request = request;
+    address.is_default = is_default;
+    return await this.addressRepository.save(address);
+  }
+
+  // 사용자 주소 삭제
+  async deleteAddress(address_key: number): Promise<void> {
+    const address = await this.addressRepository.findOneBy({ address_key });
+    if (!address) {
+      throw new Error("해당 주소를 찾을 수 없습니다.");
+    }
+    await this.addressRepository.remove(address);
+    return;
+  }
 }
