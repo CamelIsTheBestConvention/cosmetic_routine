0fd90dd9f7fb8c35bef8ad6c7e7a94c4ead1c672
CamelIsTheBestConvention <ansdj3523@gmail.com>
2024-09-27 17:21:20 +0900

    refixing infinity rendering

--- corou-frontend/src/components/detail/detailTag.tsx
+++ corou-frontend/src/components/detail/detailTag.tsx
@@ -3,34 +3,11 @@ import "../../scss/detail/detailInfo.scss";
 import axios from "axios";
 
 interface detailTag {
-  tag: number[];
   tagData: string[];
-  setTagData: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-const DetailTag: React.FC<detailTag> = ({ tag, tagData, setTagData }) => {
-  // const [tagData, setTagData] = useState<string[]>([]);
-  const backPort = process.env.REACT_APP_BACKEND_PORT;
-  console.log(tag);
-
-  useEffect(() => {
-    const fetchTagData = async () => {
-      try {
-        const tagName = tag.map(async (tag_key) => {
-          const response = await axios.get(`${backPort}/api/tag/${tag_key}`);
-          return response.data;
-        });
-
-        const result = await Promise.all(tagName);
-        setTagData(result);
-        console.log(result);
-      } catch (error) {
-        console.error("태그 데이터 가져오기 중 오류");
-      }
-    };
-
-    fetchTagData();
-  }, [tag]);
+const DetailTag: React.FC<detailTag> = ({ tagData }) => {
+  console.log(tagData);
 
   return (
     <>
